7ef1d751f88a49be9ead3f1feda2c92968e9a753
DanielLichorobiec <daniel.lichorobiec@gmail.com>
2017-04-20 18:18:33 +0200

    my first commit

---
+++ src/main/java/bla bla/App.java
@@ -0,0 +1,17 @@
+package bla bla;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j <; j++) {
+
+
+            }
+        }
+        System.out.println("Hello World!");
+    }
+}
